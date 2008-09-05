24267bbd8d0a07d737dc9ca428128b5d3f3d6267
alexrobin <robin@nsstc.uah.edu>
2008-09-05 08:51:40 +0000

    Added TimeVector3d that is a Vector3d with a time tag. Note that this is different from a Vector4d in which the fourth coordinate is multiplied by 4x4 matrices!!
    git-svn-id: http://swe-common-data-framework.googlecode.com/svn/common@213 4a6684ca-5c49-0410-8270-e1cb95179ea8

--- src/org/vast/math/Vector3d.java
+++ src/org/vast/math/Vector3d.java
@@ -11,7 +11,11 @@
  
  The Original Code is the "SensorML DataProcessing Engine".
  
- The Initial Developer of the Original Code is the VAST team at the University of Alabama in Huntsville (UAH). <http://vast.uah.edu> Portions created by the Initial Developer are Copyright (C) 2007 the Initial Developer. All Rights Reserved. Please Contact Mike Botts <mike.botts@uah.edu> for more information.
+ The Initial Developer of the Original Code is the VAST team at the
+ University of Alabama in Huntsville (UAH). <http://vast.uah.edu>
+ Portions created by the Initial Developer are Copyright (C) 2007
+ the Initial Developer. All Rights Reserved.
+ Please Contact Mike Botts <mike.botts@uah.edu> for more information.
  
  Contributor(s): 
     Alexandre Robin <robin@nsstc.uah.edu>
@@ -19,7 +23,6 @@
 ******************************* END LICENSE BLOCK ***************************/
 
 package org.vast.math;
-
 
 /**
  * <p><b>Title:</b><br/>
@@ -39,206 +42,226 @@ package org.vast.math;
  */
 public class Vector3d extends javax.vecmath.Vector3d
 {
-    static final long serialVersionUID = 0;
-    protected String crs;
-    
-    
-    public Vector3d()
-    {
-        super();
-    }
+	private static final long serialVersionUID = -2905506681710125806L;
+	protected String crs;
 
 
-    public Vector3d(double[] coordinates)
-    {
-        super(coordinates);
-    }
-    
-    
-    public Vector3d(double x, double y, double z)
-    {
-        super(x, y, z);
-    }
-    
-    
-    public Vector3d(Vector3d v)
-    {
-        super(v);
-    }
+	public Vector3d()
+	{
+		super();
+	}
 
 
-    public Vector3d copy()
-    {
-        return new Vector3d(this);
-    }
+	public Vector3d(double[] coordinates)
+	{
+		super(coordinates);
+	}
 
 
-    /**
-     * Rotates vector around X axis by given angle
-     * @param angleRadians double
-     */
-    public void rotateX(double angleRadians)
-    {
-        double c,s;
-        c = Math.cos(angleRadians);
-        s = Math.sin(angleRadians);
-        Vector3d v = this.copy();
-        y = c * v.y + -s * v.z;
-        z = s * v.y + c * v.z;
-    }
+	public Vector3d(double x, double y, double z)
+	{
+		super(x, y, z);
+	}
 
 
-    /**
-     * Rotates vector around Y axis by given angle
-     * @param angleRadians double
-     */
-    public void rotateY(double angleRadians)
-    {
-        double c, s;
-        c = Math.cos(angleRadians);
-        s = Math.sin(angleRadians);
-        Vector3d v = this.copy();
-        x = c * v.x + s * v.z;
-        z = -s * v.x + c * v.z;
-    }
+	public Vector3d(Vector3d v)
+	{
+		super(v);
+		this.crs = v.crs;
+	}
 
 
-    /**
-     * Rotates vector around Z axis by given angle
-     * @param angleRadians double
-     */
-    public void rotateZ(double angleRadians)
-    {
-        double c, s;
-        c = Math.cos(angleRadians);
-        s = Math.sin(angleRadians);
-        Vector3d v = this.copy();
-        x = c * v.x - s * v.y;
-        y = s * v.x + c * v.y;
-    }
+	public Vector3d copy()
+	{
+		return new Vector3d(this);
+	}
 
 
-    /**
-     * Transforms vector using homogeneous 4x4 matrix
-     * @param m Matrix4x4
-     */
-    public void transform(Matrix4d m)
-    {
-        double x = this.x;
-        double y = this.y;
-        double z = this.z;
-        
-        this.x = m.getElement(0, 0) * x + m.getElement(0, 1) * y
-            + m.getElement(0, 2) * z + m.getElement(0, 3);
-
-        this.y = m.getElement(1, 0) * x + m.getElement(1, 1) * y
-            + m.getElement(1, 2) * z + m.getElement(1, 3);
-
-        this.z = m.getElement(2, 0) * x + m.getElement(2, 1) * y
-            + m.getElement(2, 2) * z + m.getElement(2, 3);
-    }
+	/**
+	 * Rotates vector around X axis by given angle
+	 * @param angleRadians double
+	 */
+	public void rotateX(double angleRadians)
+	{
+		double c, s;
+		c = Math.cos(angleRadians);
+		s = Math.sin(angleRadians);
+		Vector3d v = this.copy();
+		y = c * v.y + -s * v.z;
+		z = s * v.y + c * v.z;
+	}
 
 
-
-    /**
-     * Rotates vector using 3x3 rotation matrix
-     * @param m Matrix4x4
-     */
-    public void rotate(Matrix3d m)
-    {
-        double x = this.x;
-        double y = this.y;
-        double z = this.z;
-
-        this.x = m.getElement(0, 0) * x + m.getElement(0, 1) * y
-            + m.getElement(0, 2) * z;
-
-        this.y = m.getElement(1, 0) * x + m.getElement(1, 1) * y
-            + m.getElement(1, 2) * z;
-
-        this.z = m.getElement(2, 0) * x + m.getElement(2, 1) * y
-            + m.getElement(2, 2) * z;
-    }
+	/**
+	 * Rotates vector around Y axis by given angle
+	 * @param angleRadians double
+	 */
+	public void rotateY(double angleRadians)
+	{
+		double c, s;
+		c = Math.cos(angleRadians);
+		s = Math.sin(angleRadians);
+		Vector3d v = this.copy();
+		x = c * v.x + s * v.z;
+		z = -s * v.x + c * v.z;
+	}
 
 
-    /**
-     * Rotates vector utilizing the 3x3 portion of a 4x4 matrix
-     * @param m Matrix4x4
-     */
-    public void rotate(Matrix4d m)
-    {
-        double x = this.x;
-        double y = this.y;
-        double z = this.z;
-
-        this.x = m.getElement(0, 0) * x + m.getElement(0, 1) * y
-            + m.getElement(0, 2) * z;
-
-        this.y = m.getElement(1, 0) * x + m.getElement(1, 1) * y
-            + m.getElement(1, 2) * z;
-
-        this.z = m.getElement(2, 0) * x + m.getElement(2, 1) * y
-            + m.getElement(2, 2) * z;
-    }
-    
-    
-    /**
-     * Rotates vector using given quaternion as axis and angle
-     * @param q Quaternion
-     */
-    public void rotate(Quat4d q)
-    {
-        if (q.isNull())
-            return;
-        
-        q = q.copy();
-        Quat4d v = new Quat4d(this);
-        Quat4d qT = q.copy();
-        qT.conjugate();
-        
-        v.mul(qT);
-        q.mul(v);
- 
-        x = q.x;
-        y = q.y;
-        z = q.z;
-    }
-    
-    // subtract vector 2 from vector1 and put return resulting vector
-    public final static Vector3d subtract(Vector3d v1, Vector3d v2){
-    	Vector3d v3 = new Vector3d(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);	
-    	return v3;
-    }
-
-    // subtract vector 2 from vector1 and put return resulting vector
-    public void subtract(Vector3d v2){
-    	this.x = this.x - v2.x;
-    	this.y = this.y - v2.y;
-    	this.z = this.z - v2.z;
-     }
-
-    // subtract vector 2 from vector1 and put return resulting vector
-    public final static Vector3d add(Vector3d v1, Vector3d v2){
-    	Vector3d v3 = new Vector3d(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);	
-    	return v3;
-    }
-
-    // subtract vector 2 from vector1 and put return resulting vector
-    public void add(Vector3d v2){
-    	this.x = this.x + v2.x;
-    	this.y = this.y + v2.y;
-    	this.z = this.z + v2.z;
-     }
-    public String getCrs()
-    {
-        return crs;
-    }
+	/**
+	 * Rotates vector around Z axis by given angle
+	 * @param angleRadians double
+	 */
+	public void rotateZ(double angleRadians)
+	{
+		double c, s;
+		c = Math.cos(angleRadians);
+		s = Math.sin(angleRadians);
+		Vector3d v = this.copy();
+		x = c * v.x - s * v.y;
+		y = s * v.x + c * v.y;
+	}
 
 
-    public void setCrs(String crs)
-    {
-        this.crs = crs;
-    }
-    
-    
+	/**
+	 * Transforms vector using homogeneous 4x4 matrix
+	 * @param m Matrix4x4
+	 */
+	public void transform(Matrix4d m)
+	{
+		double x = this.x;
+		double y = this.y;
+		double z = this.z;
+
+		this.x = m.getElement(0, 0) * x +
+		         m.getElement(0, 1) * y +
+		         m.getElement(0, 2) * z +
+		         m.getElement(0, 3);
+
+		this.y = m.getElement(1, 0) * x +
+		         m.getElement(1, 1) * y +
+		         m.getElement(1, 2) * z +
+		         m.getElement(1, 3);
+
+		this.z = m.getElement(2, 0) * x +
+		         m.getElement(2, 1) * y +
+		         m.getElement(2, 2) * z +
+		         m.getElement(2, 3);
+	}
+
+
+	/**
+	 * Rotates vector using 3x3 rotation matrix
+	 * @param m Matrix4x4
+	 */
+	public void rotate(Matrix3d m)
+	{
+		double x = this.x;
+		double y = this.y;
+		double z = this.z;
+
+		this.x = m.getElement(0, 0) * x +
+		         m.getElement(0, 1) * y +
+		         m.getElement(0, 2) * z;
+
+		this.y = m.getElement(1, 0) * x +
+		         m.getElement(1, 1) * y +
+		         m.getElement(1, 2) * z;
+
+		this.z = m.getElement(2, 0) * x +
+		         m.getElement(2, 1) * y +
+		         m.getElement(2, 2) * z;
+	}
+
+
+	/**
+	 * Rotates vector utilizing the 3x3 portion of a 4x4 matrix
+	 * @param m Matrix4x4
+	 */
+	public void rotate(Matrix4d m)
+	{
+		double x = this.x;
+		double y = this.y;
+		double z = this.z;
+
+		this.x = m.getElement(0, 0) * x +
+				 m.getElement(0, 1) * y +
+				 m.getElement(0, 2) * z;
+
+		this.y = m.getElement(1, 0) * x +
+		         m.getElement(1, 1) * y +
+		         m.getElement(1, 2) * z;
+
+		this.z = m.getElement(2, 0) * x +
+		         m.getElement(2, 1) * y +
+		         m.getElement(2, 2) * z;
+	}
+
+
+	/**
+	 * Rotates vector using given quaternion as axis and angle
+	 * @param q Quaternion
+	 */
+	public void rotate(Quat4d q)
+	{
+		if (q.isNull())
+			return;
+
+		q = q.copy();
+		Quat4d v = new Quat4d(this);
+		Quat4d qT = q.copy();
+		qT.conjugate();
+
+		v.mul(qT);
+		q.mul(v);
+
+		x = q.x;
+		y = q.y;
+		z = q.z;
+	}
+
+
+	// subtract vector 2 from vector1 and return resulting vector
+	public final static Vector3d subtract(Vector3d v1, Vector3d v2)
+	{
+		Vector3d v3 = new Vector3d(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+		return v3;
+	}
+
+
+	// subtract vector 2 from this vector1
+	public void subtract(Vector3d v2)
+	{
+		this.x = this.x - v2.x;
+		this.y = this.y - v2.y;
+		this.z = this.z - v2.z;
+	}
+
+
+	// add vector 2 to vector1 and return resulting vector
+	public final static Vector3d add(Vector3d v1, Vector3d v2)
+	{
+		Vector3d v3 = new Vector3d(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+		return v3;
+	}
+
+
+	// add vector 2 to this vector
+	public void add(Vector3d v2)
+	{
+		this.x = this.x + v2.x;
+		this.y = this.y + v2.y;
+		this.z = this.z + v2.z;
+	}
+
+
+	public String getCrs()
+	{
+		return crs;
+	}
+
+
+	public void setCrs(String crs)
+	{
+		this.crs = crs;
+	}
 }
