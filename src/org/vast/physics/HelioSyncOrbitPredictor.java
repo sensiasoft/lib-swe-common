6b70a91783114125b27ccf7ca70f2b148c019116
sensiasoft <alex.robin@sensiasoftware.com>
2008-09-30 09:53:11 +0000

    Updated license (also testing commit mailing list)
    git-svn-id: http://swe-common-data-framework.googlecode.com/svn/trunk@229 4a6684ca-5c49-0410-8270-e1cb95179ea8

--- src/org/vast/physics/HelioSyncOrbitPredictor.java
+++ src/org/vast/physics/HelioSyncOrbitPredictor.java
@@ -1,3 +1,28 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with
+ the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1.1.html
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+ 
+ The Original Code is the "SensorML DataProcessing Engine".
+ 
+ The Initial Developer of the Original Code is the VAST team at the
+ University of Alabama in Huntsville (UAH). <http://vast.uah.edu>
+ Portions created by the Initial Developer are Copyright (C) 2007
+ the Initial Developer. All Rights Reserved.
+
+ Please Contact Mike Botts <mike.botts@uah.edu> for more information.
+ 
+ Contributor(s): 
+    Alexandre Robin <alexandre.robin@spotimage.fr>
+ 
+******************************* END LICENSE BLOCK ***************************/
+
 package org.vast.physics;
 
 import org.vast.math.Vector3d;
@@ -6,16 +31,17 @@ import org.vast.util.DateTimeFormat;
 
 /**
  * <p><b>Title:</b><br/>
- * OrbitPredictor
+ * HelioSyncOrbitPredictor
  * </p>
  *
  * <p><b>Description:</b><br/>
- * Helper class to compute satellite orbit position/velocity
+ * Helper class to compute heliosynchronous satellite
+ * position/velocity on nominal orbit 
  * </p>
  *
  * <p>Copyright (c) 2005</p>
  * @author Alexandre Robin
- * @since Feb 25, 2003
+ * @since Feb 25, 2008
  * @version 1.0
  */
 public class HelioSyncOrbitPredictor extends AbstractOrbitPredictor
