d5985d1f51436fc8a6341681ba6a60d4c20be8fb
sensiasoft <alex.robin@sensiasoftware.com>
2013-03-20 21:51:35 +0000

    Removed old classes
    git-svn-id: http://swe-common-data-framework.googlecode.com/svn/trunk@333 4a6684ca-5c49-0410-8270-e1cb95179ea8

--- src/org/vast/ogc/gml/GMLException.java
+++
@@ -1,47 +0,0 @@
-/***************************** BEGIN LICENSE BLOCK ***************************
-
- The contents of this file are subject to the Mozilla Public License Version
- 1.1 (the "License"); you may not use this file except in compliance with
- the License. You may obtain a copy of the License at
- http://www.mozilla.org/MPL/MPL-1.1.html
- 
- Software distributed under the License is distributed on an "AS IS" basis,
- WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- for the specific language governing rights and limitations under the License.
- 
- The Original Code is the "OGC Service Framework".
- 
- The Initial Developer of the Original Code is the VAST team at the University of Alabama in Huntsville (UAH). <http://vast.uah.edu> Portions created by the Initial Developer are Copyright (C) 2007 the Initial Developer. All Rights Reserved. Please Contact Mike Botts <mike.botts@uah.edu> for more information.
- 
- Contributor(s): 
-    Alexandre Robin <robin@nsstc.uah.edu>
- 
-******************************* END LICENSE BLOCK ***************************/
-
-package org.vast.ogc.gml;
-
-import org.vast.ogc.OGCException;
-
-
-public class GMLException extends OGCException
-{
-	static final long serialVersionUID = 0;
-		
-    
-	public GMLException(String message)
-	{
-		super(message);
-	}
-	
-    
-	public GMLException(Exception e)
-	{
-		super(e);
-	}
-	
-    
-	public GMLException(String message, Exception e)
-	{
-		super(message, e);
-	}
-}
