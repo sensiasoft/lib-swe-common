cfba06f25fff82ce65ce6740ef21be4661f11076
sensiasoft <alex.robin@sensiasoftware.com>
2013-09-08 21:41:12 +0000

    Moved test code to Maven src/test/java folder
    git-svn-id: http://swe-common-data-framework.googlecode.com/svn/trunk@346 4a6684ca-5c49-0410-8270-e1cb95179ea8

---
+++ swe-common-core/src/test/java/org/vast/sweCommon/TestDecompression.java
@@ -0,0 +1,121 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with
+ the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1.1.html
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+ 
+ The Original Code is the "SensorML DataProcessing Engine".
+ 
+ The Initial Developer of the Original Code is the
+ University of Alabama in Huntsville (UAH).
+ Portions created by the Initial Developer are Copyright (C) 2006
+ the Initial Developer. All Rights Reserved.
+ 
+ Contributor(s): 
+    Alexandre Robin <alex.robin@sensiasoftware.com>
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.vast.sweCommon;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import jj2000.j2k.decoder.Decoder;
+import jj2000.j2k.io.RandomAccessIO;
+import jj2000.j2k.util.ISRandomAccessIO;
+import jj2000.j2k.util.ParameterList;
+import junit.framework.TestCase;
+
+import org.vast.cdm.common.DataBlock;
+import org.vast.cdm.common.DataType;
+import org.vast.data.DataArray;
+import org.vast.data.DataGroup;
+import org.vast.data.DataValue;
+import org.vast.decompression.JP2KDecoder;
+import org.vast.decompression.JP2KStreamDecoder;
+
+
+public class TestDecompression extends TestCase
+{
+    
+    protected static ParameterList getDefaultParameters()
+    {
+        String[][] param = Decoder.getAllParameters();
+        ParameterList defpl = new ParameterList();
+        
+        for (int i=param.length-1; i>=0; i--)
+        {
+            if (param[i][3] != null)
+                defpl.put(param[i][0], param[i][3]);
+        }
+
+        // Create parameter list using defaults
+        return new ParameterList(defpl);
+    }
+    
+    public void testJP2KDecompression() throws Exception {
+    	InputStream is = getClass().getResourceAsStream("/file1.jp2");
+    	RandomAccessIO raio = new ISRandomAccessIO(is);
+    	JP2KDecoder decoder = new JP2KDecoder();
+    	byte [][] decodedImage = decoder.decode(raio);
+    	int bandSize = 768 * 512;
+    	assertEquals("Wrong number of bands returned.", 3, decodedImage.length);
+    	assertNotNull("Band 0 is null.", decodedImage[0]);
+    	assertEquals("Wrong size of decoded image.", bandSize, decodedImage[0].length);
+    	assertNotNull("Band 1 is null.", decodedImage[1]);
+    	assertEquals("Wrong size of decoded image.", bandSize, decodedImage[1].length);
+    	assertNotNull("Band 2 is null.", decodedImage[2]);
+    	assertEquals("Wrong size of decoded image.", bandSize, decodedImage[2].length);
+    }
+    
+    // The following method was disabled by Chris Dillard on 2009-12-09 for these reasons:
+    //   1. Hard coded file path is bad.  (Maven runs JUnit tests by default.)
+    //   2. Test seems invalid any way... throws NullPointerExceptions.
+    // Put in a simple replacement above.
+    public void DISABLED_testJP2KDecompression() throws Exception
+    {
+        String filePath = "F:\\Data\\ErdasTigerShark\\2008-07-21\\images\\A121665541042.jp2";//.small.jpc";
+        File file = new File(filePath);
+        FileInputStream is = new FileInputStream(file);
+
+        DataArray imgData = new DataArray(new DataValue(DataType.INT), true);
+        DataGroup pixelData = new DataGroup(3);
+        pixelData.addComponent(new DataValue("red", DataType.INT));
+        pixelData.addComponent(new DataValue("green", DataType.INT));
+        pixelData.addComponent(new DataValue("blue", DataType.INT));
+        imgData.addComponent("pixel", pixelData);
+        
+        JP2KStreamDecoder decoder = new JP2KStreamDecoder(file.length());
+        decoder.decode(new DataInputStreamBI(is), imgData);
+        System.out.println(imgData.getData());
+        
+        BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream("D:\\subset.raw"));
+        for (int p=0; p<imgData.getComponentCount(); p++)
+        {
+            DataBlock dataBlk = imgData.getComponent(p).getData();
+            
+            /*int tmp1 = dataBlk.getIntValue(0)+128;
+            int tmp2 = dataBlk.getIntValue(1)+128;
+            int tmp3 = dataBlk.getIntValue(2)+128;
+            
+            os.write(tmp1 > 255 ? 255 : (tmp1 < 0 ? 0 : tmp1));
+            os.write(tmp2 > 255 ? 255 : (tmp2 < 0 ? 0 : tmp2));
+            os.write(tmp3 > 255 ? 255 : (tmp3 < 0 ? 0 : tmp3));*/
+            os.write(dataBlk.getByteValue(0));
+            os.write(dataBlk.getByteValue(1));
+            os.write(dataBlk.getByteValue(2));
+        }        
+        os.close();
+    }
+    
+    
+}
