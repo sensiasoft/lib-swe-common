7ce8ec6b6c4fcfbff4d712a0cade8de276fcf529
sensiasoft <alex.robin@sensiasoftware.com>
2015-10-09 23:56:33 +0200

    Added filtered writer for writing only certain data components values (used in SOS to filter by observed properties)

---
+++ swe-common-core/src/main/java/org/vast/swe/FilteredWriter.java
@@ -0,0 +1,136 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+The contents of this file are subject to the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the License.
+ 
+Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.vast.swe;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.HashSet;
+import net.opengis.swe.v20.DataComponent;
+import net.opengis.swe.v20.DataEncoding;
+import net.opengis.swe.v20.ScalarComponent;
+
+
+/**
+ * <p>
+ * Delegating writer for writing only the selected components.<br/>
+ * This is used to implement filtering by observed property in SOS. 
+ * </p>
+ *
+ * @author Alex Robin <alex.robin@sensiasoftware.com>
+ * @since Oct 9, 2015
+ */
+public class FilteredWriter extends AbstractDataWriter
+{
+    AbstractDataWriter writer;
+    HashSet<String> enabledDefUris;
+    Record enabledRecord;
+    
+    
+    public FilteredWriter(AbstractDataWriter writer, Collection<String> enabledDefUris)
+    {
+        this.writer = writer;
+        this.enabledDefUris = new HashSet<String>();
+        this.enabledDefUris.add(SWEConstants.DEF_SAMPLING_TIME);
+        this.enabledDefUris.addAll(enabledDefUris);
+    }
+
+    
+    @Override
+    protected void processAtom(ScalarComponent component) throws IOException
+    {
+        if (isComponentEnabled(component))
+            writer.processAtom(component);
+    }
+
+
+    @Override
+    protected boolean processBlock(DataComponent component) throws IOException
+    {
+        if (isComponentEnabled(component))
+            enabledRecord = currentRecord;
+        
+        return writer.processBlock(component);
+    }
+    
+    
+    private boolean isComponentEnabled(DataComponent component)
+    {
+        if (enabledRecord != null && componentStack.contains(enabledRecord))
+            return true;
+        
+        String defUri = component.getDefinition();
+        
+        if (enabledDefUris != null && defUri != null)
+        {
+            if (!enabledDefUris.contains(defUri))
+                return false;
+        }
+        
+        return true;
+    }
+    
+    
+    @Override
+    protected void endDataBlock() throws Exception
+    {
+        writer.endDataBlock();
+    }
+    
+    
+    @Override
+    public void setDataComponents(DataComponent dataInfo)
+    {
+        super.setDataComponents(dataInfo);
+        writer.setDataComponents(dataInfo);
+    }
+
+
+    @Override
+    public void setDataEncoding(DataEncoding dataEncoding)
+    {
+        super.setDataEncoding(dataEncoding);
+        writer.setDataEncoding(dataEncoding);
+    }
+
+
+    @Override
+    public void setOutput(OutputStream os) throws IOException
+    {
+        writer.setOutput(os);
+    }
+
+
+    @Override
+    public void close() throws IOException
+    {
+        writer.close();        
+    }
+
+
+    @Override
+    public void flush() throws IOException
+    {
+        writer.flush();
+    }
+
+
+    @Override
+    public void reset()
+    {
+        super.reset();
+        writer.reset();
+    }
+}
