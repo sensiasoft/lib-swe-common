efc11405cef1b7a994020a32162b1b2a4461ca24
Alex Robin <alex.robin@sensiasoft.com>
2020-04-25 19:20:42 +0200

    Cache previous index to greatly speed up iterating through block list

--- swe-common-core/src/main/java/org/vast/data/DataBlockList.java
+++ swe-common-core/src/main/java/org/vast/data/DataBlockList.java
@@ -40,6 +40,14 @@ public class DataBlockList extends AbstractDataBlock
 	protected int blockIndex;
 	protected int localIndex;
 	protected boolean equalBlockSize;
+	ThreadLocal<CachedIndex> cachedIndex = new ThreadLocal<>();
+	
+	static class CachedIndex
+	{
+	    int lastIndex; // last requested index
+	    int cumulIndex;
+	    int blockIndex;
+	}
     
     
     public DataBlockList()
@@ -171,20 +179,34 @@ public class DataBlockList extends AbstractDataBlock
 		}
 		else
 		{
-	    	int size;
-			int cumul = 0;
-			int i = 0;
+	    	// use thread local cached index to speed up sequential scans
+			CachedIndex cachedIdx = cachedIndex.get();
+			if (cachedIdx == null || index <= cachedIdx.lastIndex)
+			{
+			    cachedIdx = new CachedIndex();
+			    cachedIndex.set(cachedIdx);
+			}
+			
+			int size = 0;
+            int cumul = cachedIdx.cumulIndex;
+            int i = cachedIdx.blockIndex;
 	
-			do
+			while (desiredIndex >= cumul)
 			{
 				size = blockList.get(i).getAtomCount();
 				cumul += size;
 				i++;
 			}
-			while (desiredIndex >= cumul);
 	
+			// actually use previous block because we went one block too far
+			cumul -= size;
 			blockIndex = i - 1;
-			localIndex = desiredIndex - (cumul - size);
+			localIndex = desiredIndex - cumul;
+			
+			// save indexing variables in cache for next call
+			cachedIdx.lastIndex = index;
+			cachedIdx.cumulIndex = cumul;
+			cachedIdx.blockIndex = blockIndex;
 		}
 	}
     
