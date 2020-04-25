9fd80a36c9224d197a6e43729763a4002061c953
Alex Robin <alex.robin@sensiasoft.com>
2020-04-25 19:23:14 +0200

    Fixed logic in GeenricFeature reader (read every inline value to a String and let the API consumer handle the conversion)

--- swe-common-om/src/main/java/org/vast/ogc/gml/GMLStaxBindings.java
+++ swe-common-om/src/main/java/org/vast/ogc/gml/GMLStaxBindings.java
@@ -62,7 +62,7 @@ public class GMLStaxBindings extends XMLStreamBindings
     public GMLStaxBindings(net.opengis.gml.v32.Factory fac)
     {
         super(fac);
-        featureTypesBindings = new HashMap<QName, IFeatureStaxBindings>();        
+        featureTypesBindings = new HashMap<>();        
         nsContext.registerNamespace(NS_PREFIX_GML, net.opengis.gml.v32.bind.XMLStreamBindings.NS_URI);
         nsContext.registerNamespace(NS_PREFIX_XLINK, net.opengis.swe.v20.bind.XMLStreamBindings.XLINK_NS_URI);
     }
@@ -96,49 +96,46 @@ public class GMLStaxBindings extends XMLStreamBindings
         // also read all other properties in a generic manner
         while (reader.getEventType() != XMLStreamConstants.END_ELEMENT)
         {
-            reader.nextTag();
             QName propName = reader.getName();
             
-            if (reader.hasText())
+            // skip until next non-whitespace event
+            do {reader.next();}            
+            while (reader.isWhiteSpace());
+                        
+            int eventType = reader.getEventType();            
+            switch (eventType)
             {
-                String text = reader.getElementText();
-                Object value = null;
-                
-                if (text != null)
-                {
-                    try
-                    {
-                        value = Integer.parseInt(text);
-                    }
-                    catch (NumberFormatException e)
-                    {
-                        try
-                        {
-                            value = Double.parseDouble(text);
-                        }
-                        catch (NumberFormatException e1)
-                        {
-                            try
-                            {
-                                if (text.equalsIgnoreCase("true") || text.equalsIgnoreCase("false"))
-                                    value = Boolean.parseBoolean(text);
-                                else
-                                    this.getDateTimeFromString(text);
-                            }
-                            catch (Exception e2)
-                            {
-                                value = text.trim();
-                            }
-                        }
-                    }
+                case XMLStreamReader.CHARACTERS:
+                    String text = reader.getText();
+                    if (text != null)
+                        newFeature.setProperty(propName, text.trim());
+                    reader.nextTag();
+                    break;
                     
-                    newFeature.setProperty(propName, value);
-                }
+                case XMLStreamReader.START_ELEMENT:
+                    String objName = reader.getLocalName();
+                    if ("TimeInstant".equals(objName) ||
+                        "TimePeriod".equals(objName))
+                    {
+                        newFeature.setProperty(propName, readAbstractTimeGeometricPrimitive(reader));
+                        reader.nextTag();
+                    }
+                    else if ("Point".equals(objName) ||
+                             "LineString".equals(objName) ||
+                             "Polygon".equals(objName))
+                    {
+                        newFeature.setProperty(propName, readAbstractGeometry(reader));
+                        reader.nextTag();
+                    }
+                    else
+                        skipElementAndAllChildren(reader);
+                    break;
+                    
+                default:
+                    
             }
-            else
-            {
-                skipElementAndAllChildren(reader);
-            }
+            
+            reader.nextTag();
         }        
         
         return newFeature;
